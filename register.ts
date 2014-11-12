28bd2c159a8ac115f3a1162be63de4c5fe1239c1
sequba <sequba@gmail.com>
2014-11-12 14:15:08 +0100

    this is the dawn of our fabulous app

---
+++ register.ts
@@ -0,0 +1,5 @@
+function check_registration_form() {
+	document.getElementById('username_message').innerHTML = '<p class="ok">OK</p>';
+	document.getElementById('email_message').innerHTML = '<p class="error">trolololo</p>';
+	return false;
+}
