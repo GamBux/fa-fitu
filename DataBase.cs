23faa208703b193b971d2fd36270a40713244dd5
SanguisAter <piothermail@gmail.com>
2014-11-13 23:22:52 +0100

    Baza danych umożliwia dodawanie uzytkownika

--- DataBase.cs
+++ DataBase.cs
@@ -11,7 +11,7 @@ namespace Zwinne_Postgress
     class DataBase
     {
         // Private connection to postgres database
-        NpgsqlConnection conn;
+        NpgsqlConnection conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=password;Database=Fa-fitu_DB;");
 
         /// <summary>
         /// Checks if there exist user of given name
@@ -20,7 +20,6 @@ namespace Zwinne_Postgress
         /// <returns>true if user of username exists else returns false</returns>
         public bool existUser(string username)
         {
-            conn = new NpgsqlConnection("Server=127.0.0.1;Port=5432;User Id=postgres;Password=password;Database=Fa-fitu_DB;");
             conn.Open();
 
 
@@ -28,18 +27,47 @@ namespace Zwinne_Postgress
             NpgsqlCommand Command = new NpgsqlCommand(operation, conn);
             object user = Command.ExecuteScalar();
 
+            conn.Close();
             if (user == null) return false;
             else return true;
+
+           
+        }
+        /// <summary>
+        /// Inserts a new user to DB
+        /// </summary>
+        /// <param name="username">name of user</param>
+        /// <param name="mass">mass of user in kg</param>
+        /// <param name="activity">average minutes of activity per week</param>
+        /// <param name="age">self descriptory</param>
+        /// <returns>returns number of rows affecteds if user exists returns -1</returns>
+        public int addUser(string username, int mass, int activity, int age)
+        {
+            if (existUser(username)) return -1;
+            conn.Open();
+            
+
+            string operation = 
+                String.Format(
+                "INSERT INTO Users(uname, mass, activity, age) VALUES" + "({0}, {1}, {2}, {3})"
+                ,wrapS(username), mayNullToString(mass), mayNullToString(activity), mayNullToString(age)
+                );
+            NpgsqlCommand Command = new NpgsqlCommand(operation, conn);
+            int ret = (int)Command.ExecuteNonQuery();
+
+            conn.Close();
+          
+            return ret; 
         }
 
-        /*public bool addUser(string username, int mass, int activity, int age){
-            string massString       = mass.ToString();
-            string activityString   = activity.ToString()   
-        }
-        */
         string wrapS(string s)
         {
             return "'" + s + "'";
+        }
+
+        string mayNullToString(int i) {
+            if (i == -1) return "NULL";
+            else return i.ToString();
         }
     }
 }
