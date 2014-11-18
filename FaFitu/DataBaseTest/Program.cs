a71674c31883c0a9d801c83273373cc2e7e40904
SanguisAter <piothermail@gmail.com>
2014-11-18 15:52:57 +0100

    Updated UserRelated created project for DB testing purposes

---
+++ FaFitu/DataBaseTest/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+using FaFitu.Models;
+using FaFitu.DatabaseUtils;
+using FaFitu.Utils;
+
+namespace DataBaseTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UserModel user = new UserModel();
+
+            user.UserName      		= "Ada Ramirez"; 
+            user.ComesFrom     		= "FF:Ada.Ramirez@gamil.com"; 
+            user.Mass             	= 65; 
+            user.CaloriesToBurn   	= 1900; 
+            user.CaloriesToReceive 	= 450; 
+            user.CaloriesBurned   	= 300; 
+            user.Age              	= 35;
+            user.Password           = "QWERTY";
+            user.Gender           	= UserModel.Sex.FEMALE;
+
+            UsersRelated.AddUser(user);
+
+            user.ComesFrom = "FF:Ada.Ramirez@gmail.com";
+
+            UsersRelated.UpdateUser(user, "FF:Ada.Ramirez@gamil.com");
+        }
+    }
+}
---
+++ FaFitu/DataBaseTest/UtilS.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FaFitu.Models;
+
+namespace FaFitu.Utils
+{
+    public static class UtilS
+    {
+        public static string wrap(string a)
+        {
+            return wrap("'", a, "'");
+        }
+        public static string wrap(string a, string b)
+        {
+            return wrap(b, a, b);
+        }
+        public static string wrap(string l, string a, string r)
+        {
+            return l + a + r;
+        }
+        public static string nullToS(int a)
+        {
+            if (a == -1) return "NULL";
+            else return a.ToString();
+        }
+        public static string genderToS(UserModel.Sex gender)
+        {
+            switch (gender)
+            {
+                case UserModel.Sex.MALE:
+                    return "'MALE'";
+             
+                case UserModel.Sex.FEMALE:
+                    return "'FEMALE'";
+                
+                case UserModel.Sex.UNDEFINEDLOL:
+                    return "NULL";
+                
+                default:
+                    return "";
+            }
+        }
+        public static string map(Queue<string> q, string mapped){
+            string record = "";
+            while (q.Count > 0)
+            {
+                record += q.Dequeue();
+                if (q.Count == 0) continue;
+                record += mapped;
+            }
+
+            return record;
+        }
+
+  
+    }
+ 
+}
