bd30632fae8ada99da0293f3007b91564a2e7f6a
socumbersome <patry93k@gmail.com>
2015-01-18 16:28:00 +0100

    mocked

--- FaFitu/FaFitu.Tests/MockedDatabaseUtils/MockedFoodRepository.cs
+++ FaFitu/FaFitu.Tests/MockedDatabaseUtils/MockedFoodRepository.cs
@@ -1,58 +1,65 @@
-﻿using System;
+﻿using FaFitu.DatabaseUtils;
+using FaFitu.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 namespace FaFitu.Tests.MockedDatabaseUtils
 {
-    class MockedFoodRepository : FaFitu.DatabaseUtils.IFoodRepository
+    class MockedFoodRepository : IFoodRepository
     {
-        public int AddGroundFood(Models.GroundFoodModel m)
+        HashSet<GroundFoodModel> grounds;
+        HashSet<DishModel> dishes;
+
+
+
+        public int AddGroundFood(GroundFoodModel m)
+        {
+            grounds.Add(m);
+        }
+
+        public int AddDish(DishModel m)
         {
             throw new NotImplementedException();
         }
 
-        public int AddDish(Models.DishModel m)
+        public int UpdateFood(FoodModel m)
         {
             throw new NotImplementedException();
         }
 
-        public int UpdateFood(Models.IFoodModel m)
+        public bool DeleteFood(FoodModel m)
         {
             throw new NotImplementedException();
         }
 
-        public bool DeleteFood(Models.IFoodModel m)
+        public HashSet<Models.FoodModel> GetCustomFood(UserModel user)
         {
             throw new NotImplementedException();
         }
 
-        public HashSet<Models.IFoodModel> GetCustomFood(Models.UserModel user)
+        public HashSet<FoodModel> GetDefaultFood()
         {
             throw new NotImplementedException();
         }
 
-        public HashSet<Models.IFoodModel> GetDefaultFood()
+        public Models.FoodModel GetFoodById(int id)
         {
             throw new NotImplementedException();
         }
 
-        public Models.IFoodModel GetFoodById(int id)
+        public HashSet<FoodModel> GetFoodsWithSubsequenceInName(string partial_name)
         {
             throw new NotImplementedException();
         }
 
-        public HashSet<Models.IFoodModel> GetFoodsWithSubsequenceInName(string partial_name)
+        public HashSet<FoodModel> GetAllGroundFoods()
         {
             throw new NotImplementedException();
         }
 
-        public HashSet<Models.IFoodModel> GetAllGroundFoods()
-        {
-            throw new NotImplementedException();
-        }
-
-        public HashSet<Models.IFoodModel> GetAllDishes()
+        public HashSet<FoodModel> GetAllDishes()
         {
             throw new NotImplementedException();
         }
